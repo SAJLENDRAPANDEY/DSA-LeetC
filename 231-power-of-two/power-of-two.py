3a9f30d9a38fa12e9f5bbfa50739bcc25796c210
SAJLENDRA PANDEY <168649487+SAJLENDRAPANDEY@users.noreply.github.com>
2026-07-09 13:36:47 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (17.01%) - LeetSync

--- 231-power-of-two/power-of-two.py
+++ 231-power-of-two/power-of-two.py
@@ -4,8 +4,39 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
+        # if n==1:
+        #     return True
+        # elif n<=0 or n%2!=0:
+        #     return False
+        # return self.isPowerOfTwo(n//2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         if n==1:
             return True
-        elif n<=0 or n%2!=0:
+        if n<=0 or n%2!=0:
             return False
         return self.isPowerOfTwo(n//2)
