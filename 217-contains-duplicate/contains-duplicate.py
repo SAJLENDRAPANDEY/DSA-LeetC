dba4fc8f237ec07384526441505006142d30192d
SAJLENDRA PANDEY <168649487+SAJLENDRAPANDEY@users.noreply.github.com>
2026-07-15 12:19:42 +0530

    Time: 19 ms (66.51%) | Memory: 28.5 MB (5.15%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -4,11 +4,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        freq={}
-        for num in nums:
-            freq[num]=freq.get(num,0)+1
-        for num in freq:
-            if freq[num]>1:
-                return True
-        return False
+        # freq={}
+        # for num in nums:
+        #     freq[num]=freq.get(num,0)+1
+        # for num in freq:
+        #     if freq[num]>1:
+        #         return True
+        # return False
         
+        seen={}
+        for num in nums:
+            if num in seen:
+                return True
+            seen[num]=num
+        return False
