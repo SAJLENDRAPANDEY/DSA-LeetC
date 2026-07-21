7e2a0f807f11ab4fd6ca8b9753445b7633695e1f
SAJLENDRA PANDEY <168649487+SAJLENDRAPANDEY@users.noreply.github.com>
2026-07-21 12:28:33 +0530

    Time: 140 ms (17.46%) | Memory: 19 MB (49.40%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -5,13 +5,46 @@ class Solution(object):
         :rtype: int
         """
 
-        min_n=float('inf')
-        max_n=0
-        for num in  prices:
-            min_n=min(min_n,num)
+        # min_n=float('inf')
+        # max_n=0
+        # for num in  prices:
+        #     min_n=min(min_n,num)
 
-            profit=num-min_n
+        #     profit=num-min_n
         
-            max_n=max(max_n,profit)
-        return max_n
+        #     max_n=max(max_n,profit)
+        # return max_n
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        min_p=float('inf')
+        max_p=0
+        for num in prices:
+            min_p=min(min_p,num)
+            profit=num-min_p
+            max_p=max(max_p,profit)
+        return max_p
         
