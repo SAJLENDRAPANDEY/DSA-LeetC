9245b44c200b1024f0c0a85f99b073ae1aab3227
SAJLENDRA PANDEY <168649487+SAJLENDRAPANDEY@users.noreply.github.com>
2026-08-03 10:19:04 +0530

    Time: 7 ms (54.40%) | Memory: 13.4 MB (13.21%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.py
+++ 724-find-pivot-index/find-pivot-index.py
@@ -5,15 +5,44 @@ class Solution(object):
         :rtype: int
         """
        
+        # total=sum(nums)
+        # left_sum=0
+        # right_sum=0
+        # for i in range(len(nums)):
+            
+        #     right_sum=total-nums[i]-left_sum
+        #     if left_sum==right_sum:
+        #         return i
+        #     else:
+        #         left_sum+=nums[i]
+        # return -1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         total=sum(nums)
         left_sum=0
         right_sum=0
+
         for i in range(len(nums)):
-            
             right_sum=total-nums[i]-left_sum
             if left_sum==right_sum:
                 return i
             else:
                 left_sum+=nums[i]
         return -1
+
             
