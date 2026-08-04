0dc50d62d1aa0e2bc086a65ef6338b207d3fc00f
SAJLENDRA PANDEY <168649487+SAJLENDRAPANDEY@users.noreply.github.com>
2026-08-04 12:53:42 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (16.49%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -56,6 +56,26 @@ class Solution(object):
 
 
 
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i]+nums[j]==target:
+                    return [i,j]
+                
+                i+=1
+                j+=1
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
