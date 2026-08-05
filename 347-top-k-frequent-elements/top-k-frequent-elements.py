c4680fc6b000eccc56a53c0716ac35b6e99cf745
SAJLENDRA PANDEY <168649487+SAJLENDRAPANDEY@users.noreply.github.com>
2026-08-05 10:23:00 +0530

    Time: 11 ms (56.41%) | Memory: 14.3 MB (57.52%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        freq={}
+        for num in nums:
+            freq[num]=freq.get(num,0)+1
+        sorted_fre=sorted(freq.items(),key=lambda x:x[1] ,reverse=True)
+        res=[]
+        for x in range(k):
+            res.append(sorted_fre[x][0])
+        return res
