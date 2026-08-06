c8f19cd58d4993cbe44c6fbfb23218e594f7098d
SAJLENDRA PANDEY <168649487+SAJLENDRAPANDEY@users.noreply.github.com>
2026-08-06 09:33:22 +0530

    Time: 7 ms (81.71%) | Memory: 14.3 MB (57.55%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -5,11 +5,43 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
+        # freq={}
+        # for num in nums:
+        #     freq[num]=freq.get(num,0)+1
+        # sorted_fre=sorted(freq.items(),key=lambda x:x[1] ,reverse=True)
+        # res=[]
+        # for x in range(k):
+        #     res.append(sorted_fre[x][0])
+        # return res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         freq={}
         for num in nums:
             freq[num]=freq.get(num,0)+1
-        sorted_fre=sorted(freq.items(),key=lambda x:x[1] ,reverse=True)
+        sorted_num=sorted(freq.items(),key=lambda x:x[1],reverse=True)
         res=[]
-        for x in range(k):
-            res.append(sorted_fre[x][0])
+        for i in range(k):
+            res.append(sorted_num[i][0])
         return res
