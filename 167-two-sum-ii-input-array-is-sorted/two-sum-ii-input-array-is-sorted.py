ad325cb3e4d1470f546500c55b58aaecc40da6fa
SAJLENDRA PANDEY <168649487+SAJLENDRAPANDEY@users.noreply.github.com>
2026-08-07 09:22:10 +0530

    Time: 7 ms (50.34%) | Memory: 12.9 MB (13.05%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -5,20 +5,52 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
+        # left=0
+        # right=len(numbers)-1
+        # # for i in range(len(numbers)):
+        # #     for j in range(i+1,len(numbers)):
+        # #         if numbers[i]+numbers[j]==target:
+        # #             return i+1,j+1
+        # #     i+=1
+        # #     j+=1
+
+        # while left<right:
+        #     mid=numbers[left]+numbers[right]
+        #     if mid==target:
+        #         return [left+1,right+1]
+        #     elif mid<target:
+        #         left+=1
+        #     else:
+        #         right-=1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         left=0
         right=len(numbers)-1
-        # for i in range(len(numbers)):
-        #     for j in range(i+1,len(numbers)):
-        #         if numbers[i]+numbers[j]==target:
-        #             return i+1,j+1
-        #     i+=1
-        #     j+=1
-
         while left<right:
             mid=numbers[left]+numbers[right]
             if mid==target:
-                return [left+1,right+1]
+                return left+1,right+1
             elif mid<target:
                 left+=1
             else:
                 right-=1
+                
