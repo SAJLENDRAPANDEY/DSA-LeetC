347916ef50076e533cc25176f0b6a1a641ab723f
SAJLENDRA PANDEY <168649487+SAJLENDRAPANDEY@users.noreply.github.com>
2026-08-08 10:19:37 +0530

    Time: 10 ms (16.17%) | Memory: 12.9 MB (45.38%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -43,6 +43,25 @@ class Solution(object):
 
 
 
+        # left=0
+        # right=len(numbers)-1
+        # while left<right:
+        #     mid=numbers[left]+numbers[right]
+        #     if mid==target:
+        #         return left+1,right+1
+        #     elif mid<target:
+        #         left+=1
+        #     else:
+        #         right-=1
+                
+
+
+
+
+
+        
+
+
         left=0
         right=len(numbers)-1
         while left<right:
@@ -53,4 +72,3 @@ class Solution(object):
                 left+=1
             else:
                 right-=1
-                
