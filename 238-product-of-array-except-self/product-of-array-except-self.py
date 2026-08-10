62d931d8eeb45a56e5b2c8869cc1194d2ff5ed73
SAJLENDRA PANDEY <168649487+SAJLENDRAPANDEY@users.noreply.github.com>
2026-08-10 12:20:31 +0530

    Time: 31 ms (62.67%) | Memory: 20.1 MB (72.84%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -4,14 +4,36 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
+        # n=len(nums)
+        # ans=[1]*n
+        # left=1
+        # for i in range(len(nums)):
+        #     ans[i]=left
+        #     left*=nums[i]
+        # right=1
+        # for i in range(n-1,-1,-1):
+        #     ans[i]*=right
+        #     right*=nums[i]
+        # return ans
+
+
+
+
+
+
+
+
+
+
+
         n=len(nums)
         ans=[1]*n
         left=1
-        for i in range(len(nums)):
+        right=1
+        for i in range(n):
             ans[i]=left
             left*=nums[i]
-        right=1
-        for i in range(n-1,-1,-1):
+        for i in range(len(nums)-1,-1,-1):
             ans[i]*=right
             right*=nums[i]
         return ans
