3a044c2a53d9ad37508e97e467ec842e17374cd7
SAJLENDRA PANDEY <168649487+SAJLENDRAPANDEY@users.noreply.github.com>
2026-08-11 06:22:06 +0530

    Time: 67 ms (22.87%) | Memory: 30.6 MB (7.29%) - LeetSync

---
+++ 287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def findDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        freq={}
+        for num in nums:
+            freq[num]=freq.get(num,0)+1
+        for num in freq:
+            if freq[num]>=2:
+                return num
+        
