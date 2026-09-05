342bb3c76a0f3c3d2dbd405a5b230939cd42a8ca
SAJLENDRA PANDEY <168649487+SAJLENDRAPANDEY@users.noreply.github.com>
2026-09-05 12:42:23 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (57.95%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.py
+++ 874-backspace-string-compare/backspace-string-compare.py
@@ -5,19 +5,45 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
+        # stack1=[]
+        # stack2=[]
+        # for ch in s:
+        #     if ch =='#':
+        #         if stack1:
+        #             stack1.pop()
+        #     else:
+        #         stack1.append(ch)
+        # for ch in t:
+        #     if ch =='#':
+        #         if stack2:
+        #             stack2.pop()
+        #     else:
+        #         stack2.append(ch)
+                
+        # return stack1==stack2
+
+
+
+
+
+
+
+
+
+
         stack1=[]
         stack2=[]
+
         for ch in s:
-            if ch =='#':
+            if ch=="#":
                 if stack1:
                     stack1.pop()
             else:
                 stack1.append(ch)
         for ch in t:
-            if ch =='#':
+            if ch=="#":
                 if stack2:
                     stack2.pop()
             else:
                 stack2.append(ch)
-                
         return stack1==stack2
