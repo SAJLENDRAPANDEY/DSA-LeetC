30618d1d7f47634525fb70907d5b15ead80bf07e
SAJLENDRA PANDEY <168649487+SAJLENDRAPANDEY@users.noreply.github.com>
2026-09-06 16:36:36 +0530

    Time: 251 ms (64.45%) | Memory: 0B (100.00%) - LeetSync

--- 175-combine-two-tables/combine-two-tables.sql
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -1,9 +1,37 @@
 -- Write your PostgreSQL query statement below
-SELECT
+-- SELECT
+-- p.firstName,
+-- p.lastName,
+-- a.city,
+-- a.state
+-- FROM Person p
+-- LEFT JOIN Address a
+-- ON p.personId=a.personId;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+SELECT 
 p.firstName,
 p.lastName,
 a.city,
 a.state
 FROM Person p
 LEFT JOIN Address a
-ON p.personId=a.personId;
+ON p.personId=a.personId
