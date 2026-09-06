7c2757c20cf93f1bb51ea9c9abc65eb8c9a8a8ad
SAJLENDRA PANDEY <168649487+SAJLENDRAPANDEY@users.noreply.github.com>
2026-09-06 11:54:30 +0530

    Time: 8 ms (58.27%) | Memory: 13.8 MB (11.88%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -4,13 +4,44 @@ class Solution(object):
         :type tokens: List[str]
         :rtype: int
         """
+        # stack=[]
+        # for token in tokens:
+        #     if token in "+-*/":
+        #         a=stack.pop()
+        #         b=stack.pop()
+                
+        #         if  token=="+":
+        #             res=b+a
+        #         elif token=="-":
+        #             res=b-a
+        #         elif token=="*":
+        #             res=b*a
+        #         else:
+        #             res=int(float(b)/a)
+        #         stack.append(res)
+        #     else:
+        #         stack.append(int(token))
+        # return stack[-1]
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
         stack=[]
         for token in tokens:
             if token in "+-*/":
                 a=stack.pop()
                 b=stack.pop()
-                
-                if  token=="+":
+                if token=="+":
                     res=b+a
                 elif token=="-":
                     res=b-a
@@ -22,4 +53,4 @@ class Solution(object):
             else:
                 stack.append(int(token))
         return stack[-1]
-        
+            
