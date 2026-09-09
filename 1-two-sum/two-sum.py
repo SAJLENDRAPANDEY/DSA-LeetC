6e694359823b8f1b63f888321e172c982c53fb8a
SAJLENDRA PANDEY <168649487+SAJLENDRAPANDEY@users.noreply.github.com>
2026-09-09 11:47:03 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (68.19%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -33,13 +33,21 @@ class Solution(object):
         #             return (i,j)
         
 
+    #    freq={}
+    #     for index,num in enumerate(nums):
+    #         req=target-num
+    #         if req in freq:
+    #             return (freq[req],index)
+    #         freq[num]=index 
+
+
+
         freq={}
         for index,num in enumerate(nums):
             req=target-num
             if req in freq:
                 return (freq[req],index)
             freq[num]=index
-
 
 
 
