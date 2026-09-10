13dd66e1187691082f87d63b5838f1067ee483c5
SAJLENDRA PANDEY <168649487+SAJLENDRAPANDEY@users.noreply.github.com>
2026-09-10 18:03:24 +0530

    Time: 15 ms (85.59%) | Memory: 12.5 MB (86.22%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -30,18 +30,36 @@ class Solution(object):
 
 
 
+        # if len(s)!=len(t):
+        #     return False
+        # freq={}
+        # for ch in s:
+        #     freq[ch]=freq.get(ch,0)+1
+        # for ch in t:
+        #     if ch not  in  freq:
+        #         return False
+        #     freq[ch]-=1
+        #     if freq[ch]<0:
+        #         return False
+        # return True
+
+
+
+
+
         if len(s)!=len(t):
             return False
         freq={}
         for ch in s:
             freq[ch]=freq.get(ch,0)+1
         for ch in t:
-            if ch not  in  freq:
+            if ch not in freq:
                 return False
             freq[ch]-=1
             if freq[ch]<0:
                 return False
         return True
+
 
 
         
