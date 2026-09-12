6f73fbc4c4258e806913600226ec8d3a0fb2358f
SAJLENDRA PANDEY <168649487+SAJLENDRAPANDEY@users.noreply.github.com>
2026-09-12 15:14:47 +0530

    Time: 91 ms (78.18%) | Memory: 15.9 MB (18.71%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
@@ -4,6 +4,25 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
+        # freq={}
+        # for ch in s:
+        #     freq[ch]=freq.get(ch,0)+1
+        # for i in range(len(s)):
+        #     if freq[s[i]]==1:
+        #         return i
+        # return -1
+
+
+
+
+
+
+
+
+
+
+
+
         freq={}
         for ch in s:
             freq[ch]=freq.get(ch,0)+1
